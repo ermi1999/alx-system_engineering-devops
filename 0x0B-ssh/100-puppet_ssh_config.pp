946281870680fb8af25aec0ee06a42d650106599
ermi1999 <ermiyasabiye1999@gmail.com>
2024-01-20 09:50:23 -0800

    updated puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,12 @@
 # configures a client ssh file to use no password and to use ~/.ssh/school private key
-file { '~/.ssh/config':
-  content =>'Host 54.198.80.67\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no'
+file_line { 'Turn off password':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
 }
