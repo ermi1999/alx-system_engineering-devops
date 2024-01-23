619fa811719b3fafa9be55dae5879e02d5a36ea5
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-23 04:07:22 -0800

    updated puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,44 @@
 # setup a nginx to ubuntu server.
 
-package {'update':
-  ensure   => latest,
-  provider => 'apt'
+package { 'nginx':
+  ensure => installed,
 }
 
-package {'nginx':
-  ensure   => present
-  provider => 'apt'
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasstatus  => true,
+  hasrestart => true,
 }
 
-exec {"ufw allow 'Nginx HTTP'":
-  provider  => 'shell'
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
-file {'/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!'
-}
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-exec {'service nginx restart':
-  provider => 'shell'
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location / {
+		try_files ${uri} ${uri}/ =404;
+	}
+}  
+",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
