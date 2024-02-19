bc35e1e3248b3fff4546546f0cb61c21406741bd
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-19 23:14:24 +0300

    fixed gathering data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,23 @@ import sys
 
 
 if __name__ == "__main__":
-    user = requests.get("https://jsonplaceholder.typicode.com/users?id={}".format(sys.argv[1])).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(sys.argv[1])).json()
+    """this program fetches a data for a given employee id."""
+    user = requests.get(
+            "https://jsonplaceholder.typicode.com/users?id={}".format(
+                sys.argv[1])).json()
+    todos = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+                sys.argv[1])).json()
     name = user[0].get('name')
     taskDone = 0
     total = 0
     for todo in todos:
         total += 1
-        if todo.get('completed') == True:
+        if todo.get('completed') is True:
             taskDone += 1
-    print("Employee {} is done with tasks({}/{}):".format(name, taskDone, total))
+    print(
+            "Employee {} is done with tasks({}/{}):".format(
+                name, taskDone, total))
     for todo in todos:
-        print("\t {}".format(todo.get('title')))
-    
+        if todo.get('completed') is True:
+            print("\t {}".format(todo.get('title')))
