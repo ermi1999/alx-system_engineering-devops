ece15641cd49e1ff5aaa8b09447592d3b97797f0
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-19 23:59:28 +0300

    to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module fetches a data for a given employee id."""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """this program fetches a data for a given employee id."""
+    user = requests.get(
+            "https://jsonplaceholder.typicode.com/users?id={}".format(
+                sys.argv[1])).json()
+    todos = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+                sys.argv[1])).json()
+    username = user[0].get('username')
+    with open('{}.csv'.format(sys.argv[1]), 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for todo in todos:
+            writer.writerow(
+                    [sys.argv[1], username,
+                     str(todo.get('completed')),
+                     todo.get('title')])
