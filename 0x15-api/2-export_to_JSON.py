37f877b1e92126fcb942896b300992468ecb7392
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-20 00:17:11 +0300

    to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """This module fetches a data for a given employee id."""
+import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
     """this program fetches a data for a given employee id."""
+    userId = sys.argv[1]
     user = requests.get(
             "https://jsonplaceholder.typicode.com/users?id={}".format(
-                sys.argv[1])).json()
+                userId)).json()
     todos = requests.get(
             "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-                sys.argv[1])).json()
+                userId)).json()
     name = user[0].get('name')
-    taskDone = 0
-    total = 0
+    _dict = {userId: []}
     for todo in todos:
-        total += 1
-        if todo.get('completed') is True:
-            taskDone += 1
-    print(
-            "Employee {} is done with tasks({}/{}):".format(
-                name, taskDone, total))
-    for todo in todos:
-        if todo.get('completed') is True:
-            print("\t {}".format(todo.get('title')))
+        data = {"task": todo.get('title'), "completed": todo.get('completed'), "username": user[0].get('username')}
+        _dict.get(userId).append(data)
+    with open("{}.json".format(userId), 'w') as f:
+        f.write(json.dumps(_dict))
+
