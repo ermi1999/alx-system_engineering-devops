02dafa560aba510b49843b983c3554acff4668c1
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-05 21:47:18 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,8 @@ def top_ten(subreddit):
                 subreddit), allow_redirects=False, headers={
                     "User-Agent": "Mozilla/5.0"})
     if res.status_code != 200:
-        return 0
+        print("None")
+        return
     res = res.json()
     i = 0
     for post in res.get('data').get('children'):
