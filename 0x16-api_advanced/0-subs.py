a0464ff01411aa2f9c27e10315851acc94ddbccc
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-05 20:37:19 +0300

    get the subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers in the subreddit."""
     req = requests.get(
             "https://www.reddit.com/r/{}/about.json".format(
-                subreddit), allow_redirects=False)
-    if req.status_code >= 300:
+                subreddit), allow_redirects=False, headers={
+                    "User-Agent": "Mozilla/5.0"})
+    if req.status_code != 200:
         return 0
     return req.json().get('data').get('subscribers')
