aff3e8c0bfac692f9af7fca7671ff29ff29fdad8
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-05 21:45:18 +0300

    top ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers in the subreddit."""
-    req = requests.get(
+    res = requests.get(
             "https://www.reddit.com/r/{}/about.json".format(
                 subreddit), allow_redirects=False, headers={
                     "User-Agent": "Mozilla/5.0"})
-    if req.status_code != 200:
+    if res.status_code != 200:
         return 0
-    return req.json().get('data').get('subscribers')
+    return res.json().get('data').get('subscribers')
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module makes a request to the reddits api."""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the top 10 posts in the subreddit"""
+    res = requests.get(
+            "https://www.reddit.com/r/{}/hot.json".format(
+                subreddit), allow_redirects=False, headers={
+                    "User-Agent": "Mozilla/5.0"})
+    if res.status_code != 200:
+        return 0
+    res = res.json()
+    i = 0
+    for post in res.get('data').get('children'):
+        if i == 10:
+            break
+        print(post.get('data').get('title'))
+        i += 1
