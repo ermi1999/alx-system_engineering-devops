8c7c85b6310e27260bbe0457fbebeb7e7d10f0f7
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-14 11:32:29 +0300

    updated task 0 provider

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,5 @@
 # updates a file limit in nginx
-exec { 'limit':
-  command  => 'sudo sed -i "/^ULIMIT=\\\"/ c\\ULIMIT=\\\"-n 2000\\\"" /etc/default/nginx',
-  provider => shell,
-}
-exec { 'restart':
-  command  => 'sudo service nginx restart',
+exec { 'Correct file and restart':
+  command  => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx && sudo service nginx restart',
   provider => shell,
 }
