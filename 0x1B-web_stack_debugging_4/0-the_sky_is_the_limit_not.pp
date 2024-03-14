e57d6a3284be5049a70887c1d52d633905cecfde
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-14 11:28:03 +0300

    the sky is not he limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # updates a file limit in nginx
 exec { 'limit':
-  command => 'sed -i "/^ULIMIT=\\\"/ c\\ULIMIT=\\\"-n 2000\\\"" /etc/default/nginx',
+  command => 'sudo sed -i "/^ULIMIT=\\\"/ c\\ULIMIT=\\\"-n 2000\\\"" /etc/default/nginx',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
 exec { 'restart':
-  command => 'service nginx restart',
+  command => 'sudo service nginx restart',
   path    => ['/bin', '/usr/bin/', '/usr/local/bin/'],
 }
